8b824b752572b75dbea6e927043ea63470e31568
Ashok Nallasivam <ashoknallasivam@gmail.com>
2023-10-29 16:23:02 +1100

    Added Pytest

---
+++ src/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello Ashton"
+
+
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+        "name": "Ashok Nallasivam",
+        "email": "ashoknallasivam@gmail.com",
+    }
+
+    preferred_name = request.args.get("preferred_name")
+    if preferred_name:
+        user_data["preferred_name"] = preferred_name
+
+    return jsonify(user_data), 200
+
+
+@app.route("/create-user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+
+    return jsonify(data), 201
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ tests/test_app.py
@@ -0,0 +1,5 @@
+from app import home
+
+
+def test_home():
+    assert home() == "Hello Ashok"
